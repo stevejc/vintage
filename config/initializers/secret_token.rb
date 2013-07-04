957d38b6a3ab7c8f5d9853a16a5d1241666fc735
Steve <stevemargc@gmail.com>
2013-07-04 18:03:53 -0400

    create rails app, added gems, installed bootstrap and added home controller and views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vintage::Application.config.secret_token = 'ef92f3af8aa88727a7b6d8bcae01e4a87b24f14a5ee2a1dbc067cad62147dd74c64bdbd0d431205e08c9388b1ff9dba55d2614d824c136d7f5c3650830469e66'
