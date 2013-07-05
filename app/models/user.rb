5331292916809708e9c34a8c7e4d76d809d9b157
Steve <stevemargc@gmail.com>
2013-07-04 23:09:33 -0400

    added password reset functionality, also add Profile and updated gitignore to house env variables

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,14 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+                    
+  
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver
+  end
 
   def generate_token(column)
     begin
