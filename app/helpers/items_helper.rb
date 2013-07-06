050996d164f4ffd7e31e0536bd5fe0e2d43ede05
Steve <stevemargc@gmail.com>
2013-07-06 12:44:10 -0400

    added a item showview

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,4 +1,23 @@
 module ItemsHelper
+  def format_quantity(quantity)
+    if quantity <= 0
+      "So sorry, this item is currently sold out!"
+    elsif quantity == 1
+      "Only 1 available"
+    else
+      "#{quantity} available."
+    end
+  end
+  
+  def display_loves(count)
+    if count == 0
+      "Be the first to favorite this item!"
+    elsif count == 1
+      "1 Person loves this item"
+    else
+      "#{count} People love this item."
+    end
+  end
   
   def display_item_status(item) 
     cc = case item.status
