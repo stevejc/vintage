f01aad0a4104382af5f77bebb11fd746746da4a8
Steve <stevemargc@gmail.com>
2013-07-10 14:47:12 -0400

    added bootbox.js to add better confirmation dialog boxes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery_ujs
 //= require jquery.Jcrop
 //= require jquery.remotipart
+//= require bootbox
 //= require twitter/bootstrap
 //= require_tree .
 
@@ -36,6 +37,51 @@ jQuery(function($) {
 	// activates twitter bootstrap popover functionality
 	$('.popout').popover({ 
 		html: true
-  });
+  });   
+
  
 })
+
+myCustomConfirmBox = function(message, callback) {
+  var options;
+  options = [
+    {
+      'label': 'Yes, Definitely',
+      'class': 'btn-danger',
+      'callback': function() {
+        if (typeof callback === 'function') {
+          return callback();
+        }
+      }
+    }, {
+      'label': 'Opss! No',
+      'class': 'btn-success'
+    }
+  ];
+  return bootbox.dialog(message, options);
+};
+ 
+$.rails.allowAction = function(element) {
+  var answer, callback, message;
+  message = element.data("confirm");
+  if (!message) {
+    return true;
+  }
+  answer = false;
+  callback = void 0;
+  if ($.rails.fire(element, "confirm")) {
+    myCustomConfirmBox(message, function() {
+      var oldAllowAction;
+      callback = $.rails.fire(element, "confirm:complete", [answer]);
+      if (callback) {
+        oldAllowAction = $.rails.allowAction;
+        $.rails.allowAction = function() {
+          return true;
+        };
+        element.trigger("click");
+        return $.rails.allowAction = oldAllowAction;
+      }
+    });
+  }
+  return false;
+};
